31109c36d104dfb59909d90bff927dc0c8945e4a
Gustavo Cunha <gustavocunha@gmail.com>
2014-10-29 10:32:46 -0300

    Setup ui and create struct

---
+++ 99RedBalloons/Balloon.swift
@@ -0,0 +1,14 @@
+//
+//  Balloon.swift
+//  99RedBalloons
+//
+//  Created by Gustavo Cunha on 10/29/14.
+//  Copyright (c) 2014 Gustavo Cunha. All rights reserved.
+//
+
+import UIKit
+
+struct Balloon {
+    var name:String = ""
+    var image:UIImage = UIImage()
+}
